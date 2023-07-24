33109f288ea00c740076706571af7a2e30896283
jdethrow <139158530+jdethrow@users.noreply.github.com>
2023-07-24 13:43:49 -0400

    Update script4.sh

--- script4.sh
+++ script4.sh
@@ -2,8 +2,8 @@
 input =“./names.txt”   
 while IFS=, read -r first last; # notice the comma after the equal sign
  do
-name="$Jomus $Dethrowmus"
-username="${Jomus:0:1}$Dethrowmus"
-useradd -m $username -s /bin/bash
- done < "$input"
-tail -8 /etc/passwd
+name="$Jomus $Dethrowmus" #Makes name Jomus Dethrowmus
+username="${Jomus:0:1}$Dethrowmus" #Creates a username for Jomus Dethrowmus
+useradd -m $username -s /bin/bash #Links the user and username
+ done < "$input" #ends
+tail -8 /etc/passwd #output the last part of /etc/passwd files
