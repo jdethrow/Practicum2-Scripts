f0b1ef403c03c8b5d57152d7e564e8c4846bcc23
jdethrow <139158530+jdethrow@users.noreply.github.com>
2023-07-24 13:35:30 -0400

    Update script3.sh

--- script3.sh
+++ script3.sh
@@ -2,6 +2,6 @@
 x=10               #NOT x   =  10 no spaces
 X=20               #variables are case sensitive
 y=5                    #NULL variable
-echo "10 = $10"
-echo "20 = $20"
-echo "5 = $5"
+echo "10 = $10" #10 = 
+echo "20 = $20" #20 =
+echo "5 = $5" #5 =
