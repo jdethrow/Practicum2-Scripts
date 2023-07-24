aedce653bcd37c992c4363bf42795252c6a530c0
jdethrow <139158530+jdethrow@users.noreply.github.com>
2023-07-24 13:39:56 -0400

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # example of using arguments to a script
-echo "My first name is $Joseph"
-echo "My surname is Dethrow but with a question mark bc im not sure if i did this right"
-echo "Total number of arguments is $5 uhmum"
+echo "My first name is $Joseph" #My first name is
+echo "My surname is Dethrow but with a question mark bc im not sure if i did this right" #My surname is Dethrow but with a question mark bc im not sure if i did this right
+echo "Total number of arguments is $5 uhmum" #Total number of arguments is  uhmum
